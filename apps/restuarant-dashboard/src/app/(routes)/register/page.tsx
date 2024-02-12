df1510c2df73c2052f84e586a7000b668da0813e
Deathaker <helmibe>
2024-02-12 11:42:00 +0700

    food apps commit

---
+++ apps/restuarant-dashboard/src/app/(routes)/register/page.tsx
@@ -0,0 +1,12 @@
+import Register from "../../../shared/views/auth/register";
+
+const Page = () => {
+  return (
+    <div className="w-full h-min py-10 flex flex-col justify-center">
+      <h1 className="title">Join to Mi Tech</h1>
+      <Register />
+    </div>
+  );
+};
+
+export default Page;
